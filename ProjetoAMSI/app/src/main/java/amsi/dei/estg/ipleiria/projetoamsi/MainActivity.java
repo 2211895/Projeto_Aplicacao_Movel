ccb1ea2000c0463bce8757d6dcdf248969b9aad7
Gonçalo Neves <2211895@my.ipleiria.pt>
2022-11-29 10:22:04 +0000

    Base de projeto
    ja com algumas views e funcionalidade de menu

---
+++ ProjetoAMSI/app/src/main/java/amsi/dei/estg/ipleiria/projetoamsi/MainActivity.java
@@ -0,0 +1,69 @@
+package amsi.dei.estg.ipleiria.projetoamsi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import amsi.dei.estg.ipleiria.projetoamsi.vistas.Selecao_avaliacao;
+import amsi.dei.estg.ipleiria.projetoamsi.vistas.Selecao_mensagens;
+import amsi.dei.estg.ipleiria.projetoamsi.vistas.Selecao_nutricao;
+import amsi.dei.estg.ipleiria.projetoamsi.vistas.Selecao_salas;
+import amsi.dei.estg.ipleiria.projetoamsi.vistas.Selecao_treino;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btn_treino, btn_nutricao, btn_avaliacao, btn_salas, btn_mensagens;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_treino = findViewById(R.id.btn_treino);
+        btn_nutricao = findViewById(R.id.btn_nutricao);
+        btn_avaliacao = findViewById(R.id.btn_avaliacao);
+        btn_salas = findViewById(R.id.btn_salas);
+        btn_mensagens = findViewById(R.id.btn_mensagens);
+
+        btn_treino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            Intent intentTreino = new Intent(MainActivity.this, Selecao_treino.class);
+            startActivity(intentTreino);
+            }
+        });
+
+        btn_nutricao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentNutricao = new Intent(MainActivity.this, Selecao_nutricao.class);
+                startActivity(intentNutricao);
+            }
+        });
+
+        btn_avaliacao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentAvaliacao = new Intent(MainActivity.this, Selecao_avaliacao.class);
+                startActivity(intentAvaliacao);
+            }
+        });
+
+        btn_salas.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentSalas = new Intent(MainActivity.this, Selecao_salas.class);
+                startActivity(intentSalas);
+            }
+        }));
+
+        btn_mensagens.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentMensagens = new Intent(MainActivity.this, Selecao_mensagens.class);
+                startActivity(intentMensagens);
+            }
+        });
+    }
+}
---
+++ ProjetoAMSI/app/src/main/java/amsi/dei/estg/ipleiria/projetoamsi/vistas/Selecao_mensagens.java
@@ -0,0 +1,16 @@
+package amsi.dei.estg.ipleiria.projetoamsi.vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import amsi.dei.estg.ipleiria.projetoamsi.R;
+
+public class Selecao_mensagens extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecao_mensagens);
+    }
+}
